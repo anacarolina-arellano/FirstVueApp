c6c50d4cd4a92e285e3983f393af671584d1092f
Ana Carolina Arellano <pg20carolina@vfs.com>
2021-05-26 21:26:07 -0500

    added card component

---
+++ src/components/Card.vue
@@ -0,0 +1,90 @@
+<template>
+  <v-card
+    :loading="loading"
+    class="mx-auto my-12"
+    max-width="374"
+  >
+    <template slot="progress">
+      <v-progress-linear
+        color="deep-purple"
+        height="10"
+        indeterminate
+      ></v-progress-linear>
+    </template>
+
+    <v-img
+      class="my-img"
+      height="250"
+      src="require('@/assets/' + imageName + '')"
+    ></v-img>
+
+    <v-card-title>{{nameProduct}}</v-card-title>
+
+    <v-card-text>
+      <v-row
+        align="center"
+        class="mx-0"
+      >
+        <v-rating
+          :value="4.5"
+          color="amber"
+          dense
+          half-increments
+          readonly
+          size="14"
+        ></v-rating>
+
+        <div class="grey--text ms-4">
+          ${{price}}
+        </div>
+      </v-row>
+
+      <div class="my-4 subtitle-1">
+          {{category}}
+      </div>
+
+      <div>{{description}}</div>
+    </v-card-text>
+
+    <v-divider class="mx-4"></v-divider>
+
+    <v-card-actions class="my-actions">
+      <v-btn
+        color="deep-purple lighten-2"
+        text
+        @click="reserve"
+      >
+        {{nameProduct}} <br/> ${{price}}
+      </v-btn>
+    </v-card-actions>
+  </v-card>
+</template>
+<script>
+    import Controller from '@/mixins/controller'
+
+    class Card extends Controller {
+
+        constructor( name, subComponentList = []) {
+            super( name, subComponentList )
+            this.props = {
+              nameProduct: String,
+              price: Number,
+              category: String,
+              description: String,
+              imageName: String
+            }
+            
+        }
+    }
+
+    export default new Card('myCard');
+</script>
+<style scoped>
+.my-actions{
+    justify-content: center;
+}
+.my-img{
+    justify-content: center;
+    background-position: center;
+}
+</style>
--- src/routes/Customer.vue
+++ src/routes/Customer.vue
@@ -1,18 +1,36 @@
 <template>
-    <v-row>
-        <v-col cols="10">
-            <searchbar></searchbar> 
-        </v-col>
-        <v-col cols="2">
-            <cartButton></cartButton>
-        </v-col>
-    </v-row>
+    <div>
+        <v-row>
+            <v-col cols="10">
+                <searchbar></searchbar> 
+            </v-col>
+            <v-col cols="2">
+                <cartButton></cartButton>
+            </v-col>
+            
+        </v-row>
+        <v-row>
+            <myCard nameProduct="Blue Dress"
+                    price="50"
+                    category="Clothing"
+                    description="Beautiful dress that is perfect for 
+                    any event! Its blue color is trendy, there are
+                    only a few left! "
+                    imageName="dress.jpeg">
+            </myCard>
+
+           
+        </v-row>
+        
+    </div>
+    
 </template>
 <script>
 
     import Controller from '@/mixins/controller'
     import searchbar from '../components/Searchbar'
     import cartButton from '../components/MyCartButton'
+    import myCard from '../components/Card'
 
     class Customer extends Controller {
 
@@ -22,7 +40,8 @@
             }
             this.components = {
                 searchbar,
-                cartButton
+                cartButton,
+                myCard 
             }
         }
     }
